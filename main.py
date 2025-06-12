5c877891eef804c8c85d2158802670df7d47273f
Felipe Ferraz <51456052+ferraz07@users.noreply.github.com>
2025-06-12 19:38:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
 from database import get_connection
 from fastapi.middleware.cors import CORSMiddleware 
